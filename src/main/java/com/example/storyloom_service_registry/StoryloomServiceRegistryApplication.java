a932a415be88006118e8223158ce4c1d2f982a36
Zahiq Ibrahim <161963558+ZahiqIbrahim@users.noreply.github.com>
2026-06-09 21:11:58 +0530

    service registry setup for stroyloom

---
+++ src/main/java/com/example/storyloom_service_registry/StoryloomServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.storyloom_service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryloomServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryloomServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storyloom_service_registry/StoryloomServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storyloom_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryloomServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
